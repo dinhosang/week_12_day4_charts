dff30df601a05db37db14fff28d91ca789cd40b3
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-02-22 14:02:00 +0000

    end of paired lab

--- public/sunBurstChart.js
+++ public/sunBurstChart.js
@@ -1,8 +1,12 @@
 const SunBurstChart = function(countries){
   this.countries = countries;
   this.regions = [];
+  this.subregions = {};
+  this.countriesBySubregion = {}
   this.getRegions();
+  this.getSubRegions();
 
+  console.log(this.subregions);
 }
 
 SunBurstChart.prototype.getRegions = function(){
@@ -10,6 +14,20 @@ SunBurstChart.prototype.getRegions = function(){
     if(!this.regions.includes(country.region) && country.region !== ""){
       this.regions.push(country.region);
     }
-      
-  })
+  }.bind(this))
+}
+
+SunBurstChart.prototype.getSubRegions = function(){
+  this.regions.forEach(function(region){
+    this.findSubRegionsForRegion(region)
+  }.bind(this))
+}
+
+SunBurstChart.prototype.findSubRegionsForRegion = function(region){
+  this.subregions[region] = []
+  this.countries.forEach(function(country){
+    if(country.region === region && !this.subregions[region].includes(country.subregion) && country.subregion !== ""){
+      this.subregions[region].push(country.subregion)
+    }
+  }.bind(this))
 }
